486b87e3dc98d05934212a5fccb37bde135df13a
relieyanhilman <relieyanhilman93@gmail.com>
2022-03-25 16:02:10 +0800

    change listen PORT

--- server.js
+++ server.js
@@ -153,6 +153,6 @@ app.put('/image', (req, res) => {
         .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
