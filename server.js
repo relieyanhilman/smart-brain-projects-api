c7817bae8e5a785d4b7880ceacdbe28c45b4004f
relieyanhilman <relieyanhilman93@gmail.com>
2022-03-25 11:26:30 +0800

    change db configuration

--- server.js
+++ server.js
@@ -56,11 +56,8 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        port: 5432,
-        user: 'postgres',
-        password: 'password',
-        database: 'smart-brain'
+        connectionString: 'postgresql-rigid-52606',
+        ssl: true
     }
 });
 
