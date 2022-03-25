b31887f5fe33aa332d4ab26be37217567e982a70
relieyanhilman <relieyanhilman93@gmail.com>
2022-03-25 16:11:32 +0800

    change db connection

--- server.js
+++ server.js
@@ -57,10 +57,8 @@ const db = knex({
     // Enter your own database information here based on what you created
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'password',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
